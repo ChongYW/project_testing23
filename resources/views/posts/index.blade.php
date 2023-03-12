9c0f74d62aea7a232a0934d5aa26a720c7ec19b5
CYW <68589904+ChongYW@users.noreply.github.com>
2023-03-13 03:10:13 +0800

    v1.3

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @foreach($posts as $post)
+    <div class="row">
+        <div class="col-6 offset-3">
+            <a href="/profile/{{ $post -> user -> id }}">
+                <img class="w-100" src="/storage/{{ $post -> image }}">
+            </a>
+        </div>
+
+    </div>
+
+    <div class="row pt-2 pb-4">
+        <div class="col-6 offset-3">
+            <div>
+
+                <p>
+                    <span class="fw-bold">
+                        <a href="/profile/{{ $post -> user -> id }}">{{ $post -> user -> username }}</a>
+                    </span>
+                    {{ $post -> caption }}</p>
+
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    @endforeach
+
+
+    <div class="row">
+        <div class="d-flex justify-content-center">
+            {{ $posts -> links() }}
+        </div>
+    </div>
+
+</div>
+@endsection
